4524e77446e31506b6535e7e0754c4e5264af8e0
Akash Rajan <akashrajan93@gmail.com>
2023-10-23 08:01:00 -0500

    removed unused .js components

--- src/app/components/LinkButton.tsx
+++ src/app/components/LinkButton.tsx
@@ -1,8 +1,11 @@
 const LinkButton = ({ linkName, linkVal }) => {
+  console.log("linkName", linkName, linkVal);
   return (
-    <button className="py-3 px-20 bg-gradient-to-r from-[#00F0FF] to-[#CBFF5E] rounded-lg font-azeret font-bold hover:translate-y-1 transition-all">
-      {linkName}
-    </button>
+    <a href={`https://${linkName}.com/${linkVal}`} target="_blank">
+      <button className="py-3 px-20 w-72 bg-gradient-to-r from-[#00F0FF] to-[#CBFF5E] rounded-lg font-azeret font-bold hover:translate-y-1 transition-all">
+        {linkName.charAt(0).toUpperCase() + linkName.slice(1)}
+      </button>
+    </a>
   );
 };
 
--- src/app/user/[domain]/page.tsx
+++ src/app/user/[domain]/page.tsx
@@ -1,20 +1,22 @@
 "use client";
+import { useEffect, useState, useMem } from "react";
 import { useRouter } from "next/navigation";
+import { Connection } from "@solana/web3.js";
+const endpoint = process.env.NEXT_PUBLIC_ENDPOINT;
 import { useWallet, useConnection } from "@solana/wallet-adapter-react";
 import { useFetchRecords } from "../../../hooks/useFetchRecords";
 import LinkButton from "@/app/components/LinkButton";
 
 const UserPage = ({ params }) => {
   const router = useRouter();
-  const { connection } = useConnection();
   const domain = params.domain;
-
+  const { connection } = useConnection();
+  // const connection = new Connection(endpoint);
   const { records, pic } = useFetchRecords(connection, domain);
 
-  console.log("records", records);
   return (
     <div className="flex w-screen h-full flex-col justify-start items-center mt-10">
-      <div className="flex flex-col space-y-1">
+      <div className="flex flex-col items-center space-y-1">
         <img src={pic} className="w-28 rounded-full" />
         <h1 className="font-bold text-white font-azeret">{domain}.sol</h1>
       </div>
@@ -23,9 +25,7 @@ const UserPage = ({ params }) => {
         {records
           ? Object.entries(records).map(([key, value]) => {
               if (value !== undefined) {
-                return (
-                  <LinkButton key={key} linkName={key} linkValue={value} />
-                );
+                return <LinkButton key={key} linkName={key} linkVal={value} />;
               }
               return null;
             })
