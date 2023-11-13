3e49b4c82e24a0feb4c7d4f7c758864ee3567b90
Akash Rajan <akashrajan93@gmail.com>
2023-11-13 14:52:11 -0600

    update button click animation on link share

--- src/app/components/LinkButton.tsx
+++ src/app/components/LinkButton.tsx
@@ -2,7 +2,7 @@ const LinkButton = ({ linkName, linkVal }) => {
   console.log("linkName", linkName, linkVal);
   return (
     <a href={`https://${linkName}.com/${linkVal}`} target="_blank">
-      <button className="py-3 px-20 w-72 bg-gradient-to-r from-[#00F0FF] to-[#CBFF5E] rounded-lg font-azeret font-bold hover:translate-y-1 transition-all">
+      <button className="py-3 px-20 w-72 bg-gradient-to-r from-[#00F0FF] to-[#CBFF5E] rounded-lg font-azeret font-bold active:translate-y-1 transition-all">
         {linkName.charAt(0).toUpperCase() + linkName.slice(1)}
       </button>
     </a>
