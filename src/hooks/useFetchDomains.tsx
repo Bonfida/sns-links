df36771bd337616c5dc163c3fb5a85d3d6a11ec3
Akash Rajan <akashrajan93@gmail.com>
2023-12-07 11:11:22 -0600

    fix: types refactor: update hook names, comment out original

--- src/hooks/useFetchDomains.tsx
+++ src/hooks/useFetchDomains.tsx
@@ -1,29 +1,29 @@
-import { getAllDomains, reverseLookupBatch } from "@bonfida/spl-name-service";
-import { Connection, PublicKey } from "@solana/web3.js";
-import { useQuery, UseQueryResult } from "react-query";
+// import { getAllDomains, reverseLookupBatch } from "@bonfida/spl-name-service";
+// import { Connection, PublicKey } from "@solana/web3.js";
+// import { useQuery, UseQueryResult } from "react-query";
 
-export const useFetchDomains = (
-  connection: Connection,
-  owner: PublicKey | null
-): UseQueryResult<string[], unknown> => {
-  const fetchDomains = async (): Promise<string[]> => {
-    if (!owner) {
-      return [];
-    }
-    const serializedDomainArr: PublicKey[] = await getAllDomains(
-      connection,
-      owner
-    );
-    const domains = await reverseLookupBatch(connection, serializedDomainArr);
-    const filteredDomains: string[] = domains.filter(
-      (domain): domain is string => domain !== undefined
-    );
+// export const useFetchDomains = (
+//   connection: Connection,
+//   owner: PublicKey | null
+// ): UseQueryResult<string[], unknown> => {
+//   const fetchDomains = async (): Promise<string[]> => {
+//     if (!owner) {
+//       return [];
+//     }
+//     const serializedDomainArr: PublicKey[] = await getAllDomains(
+//       connection,
+//       owner
+//     );
+//     const domains = await reverseLookupBatch(connection, serializedDomainArr);
+//     const filteredDomains: string[] = domains.filter(
+//       (domain): domain is string => domain !== undefined
+//     );
 
-    return filteredDomains;
-  };
+//     return filteredDomains;
+//   };
 
-  return useQuery<string[], unknown>({
-    queryKey: ["domains", owner],
-    queryFn: fetchDomains,
-  });
-};
+//   return useQuery<string[], unknown>({
+//     queryKey: ["domains", owner],
+//     queryFn: fetchDomains,
+//   });
+// };
--- src/hooks/useFetchTokenizedDomains.tsx
+++ src/hooks/useFetchTokenizedDomains.tsx
@@ -6,33 +6,44 @@ import {
 import { Connection, PublicKey } from "@solana/web3.js";
 import { useQuery, UseQueryResult } from "react-query";
 
-export const useFetchTokenizedDomains = (
+export const useFetchDomains = (
   connection: Connection,
   owner: PublicKey | null
 ): UseQueryResult<string[], unknown> => {
-  const fetchTokenizedDomains = async (): Promise<string[]> => {
+  const fetchDomains = async (): Promise<string[]> => {
     if (!owner) {
       return [];
     }
 
-    const serializedTokenDomainArr = await getTokenizedDomains(
-      connection,
-      owner
-    );
+    const serializedTokenDomainArr: {
+      key: PublicKey;
+      mint: PublicKey;
+      reverse: string | undefined;
+    }[] = await getTokenizedDomains(connection, owner);
     const serializedDomainArr: PublicKey[] = await getAllDomains(
       connection,
       owner
     );
-    const domains = await reverseLookupBatch(connection, serializedDomainArr);
+    const domains: (string | undefined)[] = await reverseLookupBatch(
+      connection,
+      serializedDomainArr
+    );
 
     const tokenDomains: string[] = serializedTokenDomainArr.map(
       (tokenizedDomain) => tokenizedDomain.reverse!
     );
-    domains.concat(tokenDomains);
+
+    const combinedDomains: (string | undefined)[] =
+      domains?.concat(tokenDomains);
+    const filteredDomains: string[] = combinedDomains?.filter(
+      (domain): domain is string => domain !== undefined
+    );
+
+    return filteredDomains;
   };
 
   return useQuery<string[], unknown>({
     queryKey: ["tokenizedDomains", owner],
-    queryFn: fetchTokenizedDomains,
+    queryFn: fetchDomains,
   });
 };
