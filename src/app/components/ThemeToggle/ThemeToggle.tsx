7b8c03e870bcaaada4956abbab31a8223f37869b
Akash Rajan <akashrajan93@gmail.com>
2024-04-24 13:22:03 -0500

    Fix: Initial theme state

--- src/app/components/ThemeToggle/ThemeToggle.tsx
+++ src/app/components/ThemeToggle/ThemeToggle.tsx
@@ -2,20 +2,22 @@ import { useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 export const ThemeToggle = () => {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ||
+      (window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light")
+  );
 
   useEffect(() => {
-    // Update the localStorage and document body class on theme change
     localStorage.setItem("theme", theme);
-    document.body.classList.remove("dark", "light"); // Remove all potential theme classes
-    document.body.classList.add(theme); // Add the current theme class
-  }, [theme]); // Depend on theme to rerun this effect
+    document.body.classList.remove("dark", "light");
+    document.body.classList.add(theme);
+  }, [theme]);
 
   const handleThemeToggle = () => {
     setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
   };
-
-  console.log("theme", theme);
 
   return (
     <div className="relative w-20 h-10 border-t border-t-top-border-highlight bg-glass-bg rounded-[40px]">
